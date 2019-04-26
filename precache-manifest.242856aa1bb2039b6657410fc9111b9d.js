79f3c1680a421ad354a5789266b8c21c92927333
rvemula <rajeshv1252>
2019-04-26 15:43:22 +0530

    Updates

---
+++ precache-manifest.242856aa1bb2039b6657410fc9111b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "23a7fa52cf7ad51dd85b",
+    "url": "/contactmanager/static/js/main.169abea4.chunk.js"
+  },
+  {
+    "revision": "f1aedb4b5242829c3280",
+    "url": "/contactmanager/static/js/2.1f39c293.chunk.js"
+  },
+  {
+    "revision": "23a7fa52cf7ad51dd85b",
+    "url": "/contactmanager/static/css/main.871cac56.chunk.css"
+  },
+  {
+    "revision": "f1aedb4b5242829c3280",
+    "url": "/contactmanager/static/css/2.cc5e4b7e.chunk.css"
+  },
+  {
+    "revision": "05c4811414612caa3929503e1937316b",
+    "url": "/contactmanager/index.html"
+  }
+];
